0723c0acbdfd561e054ee74d086b6980ee21fa36
Kent C. Dodds <kent@doddsfamily.us>
2018-11-14 18:48:14 -0700

    make it look nicer

--- src/index.js
+++ src/index.js
@@ -2,7 +2,19 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 
 function App() {
-  return 'look in the lessons directory'
+  return (
+    <div
+      style={{
+        padding: 30,
+        height: '100vh',
+        display: 'grid',
+        alignItems: 'center',
+        justifyContent: 'center',
+      }}
+    >
+      <div>look in the lessons directory</div>
+    </div>
+  )
 }
 
 ReactDOM.render(<App />, document.getElementById('root'))
